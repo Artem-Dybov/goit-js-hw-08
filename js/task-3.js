7384efd7a089901230117e2c5f96f5e303cb773c
Artem Dybov <dybov.artem.v@gmail.com>
2025-02-07 14:38:25 +0200

    hw-8

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const inputEl = document.querySelector("#name-input");
-const outputEl = document.querySelector("#name-output");
-inputEl.addEventListener("input", (event) => {
-  const trimmedValue = inputEl.value.trim();
-  outputEl.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
-});
